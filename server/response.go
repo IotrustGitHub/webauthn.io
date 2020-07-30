b5fd4de4db20cf8d5a7266bb57c5f65745f3b3d9
Minho.Yoo <paromix@gmail.com>
2020-07-30 22:00:54 +0900

    CORS 정책 추가

--- server/response.go
+++ server/response.go
@@ -20,6 +20,8 @@ func jsonResponse(w http.ResponseWriter, d interface{}, c int) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	
 	w.WriteHeader(c)
 	fmt.Fprintf(w, "%s", dj)
 }
