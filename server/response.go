81706d3df78a6a1997c45fa0be696c816df5c81d
Minho.Yoo <paromix@gmail.com>
2020-07-30 23:59:36 +0900

    CORS localhost 허용

--- server/response.go
+++ server/response.go
@@ -18,7 +18,7 @@ func jsonResponse(w http.ResponseWriter, d interface{}, c int) {
 		http.Error(w, "Error creating JSON response", http.StatusInternalServerError)
 	}
 	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000/")
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 	
